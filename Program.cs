acda4b5e6915a5cbd2cc507af7e5fd94b17e1717
yarin.laniado <yarinl464@gmail.com>
2023-10-16 22:33:32 +0300

    bad code

--- Program.cs
+++ Program.cs
@@ -1,72 +1,26 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("The time is " + DateTime.Now);
-Console.WriteLine("The time is " + DateTime.Now);
+//Console.WriteLine("Hello, World!");
+//Console.WriteLine("The time is " + DateTime.Now);
+public class BadCode {
+    public static void main(String[] args) {
+        // This code is difficult to read and understand.
+        int[] numbers = new int[]{1, 2, 3, 4, 5};
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] % 2 == 0) {
+                System.out.println(numbers[i]);
+            }
+        }
 
-Console.WriteLine("The time is " + DateTime.Now);
+        // This code is unnecessary complex.
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println(sum);
 
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
-
-Console.WriteLine("The time is " + DateTime.Now);
+        // This code has no comments.
+        // ...
+    }
+}
 
 
