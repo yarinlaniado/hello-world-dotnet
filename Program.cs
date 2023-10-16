7e826c415a527d5c2bb8ce067037b6878a94b91b
yarin.laniado <yarinl464@gmail.com>
2023-10-16 22:36:17 +0300

    bad code

--- Program.cs
+++ Program.cs
@@ -1,26 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-//Console.WriteLine("The time is " + DateTime.Now);
-public class BadCode {
-    public static void main(String[] args) {
+public class BadCode
+{
+    public static void Main(string[] args)
+    {
         // This code is difficult to read and understand.
         int[] numbers = new int[]{1, 2, 3, 4, 5};
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 == 0) {
-                System.out.println(numbers[i]);
+        for (int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] % 2 == 0)
+            {
+                Console.WriteLine(numbers[i]);
             }
         }
 
         // This code is unnecessary complex.
         int sum = 0;
-        for (int i = 0; i < numbers.length; i++) {
+        for (int i = 0; i < numbers.Length; i++)
+        {
             sum += numbers[i];
         }
-        System.out.println(sum);
+        Console.WriteLine(sum);
 
         // This code has no comments.
         // ...
     }
 }
-
-
